46476384c931562eb657821c3b93b61321a36779
mattyray <mnraynor90@gmail.com>
2025-02-23 12:17:04 -0500

    static settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -85,6 +85,8 @@ USE_TZ = True
 # Static Files
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 
 # Default Primary Key Field Type
